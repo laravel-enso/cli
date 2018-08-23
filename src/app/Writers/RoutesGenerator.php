9548a78ae46b6cde2e9c865e3f31dd30c23fd02c
Adrian Ocneanu <aocneanu@gmail.com>
2018-08-23 04:40:16 +0000

    Apply fixes from StyleCI

--- src/app/Writers/RoutesGenerator.php
+++ src/app/Writers/RoutesGenerator.php
@@ -28,12 +28,12 @@ class RoutesGenerator
         $resourcePrefix = $this->segments->slice(0, -1);
 
         $array = [
-            '${namespace}' => $this->namespace(),
-            '${groupPrefix}' => "->prefix('".$this->segments->implode('/')."')->as('".$this->segments->implode('.').".')",
+            '${namespace}'      => $this->namespace(),
+            '${groupPrefix}'    => "->prefix('".$this->segments->implode('/')."')->as('".$this->segments->implode('.').".')",
             '${resourcePrefix}' => $resourcePrefix->isNotEmpty()
                 ? "->prefix('".$resourcePrefix->implode('/')."')->as('".$resourcePrefix->implode('.').".')"
                 : '',
-            '${Model}' => $model,
+            '${Model}'  => $model,
             '${Models}' => str_plural($model),
             '${models}' => str_plural(lcfirst($model)),
         ];
