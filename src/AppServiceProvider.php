fbb38d1730ca61a92770fc1c2134b025565542ad
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2018-10-23 13:41:18 +0300

    added back config merging

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -7,15 +7,41 @@ use LaravelEnso\StructureManager\app\Commands\MakeEnsoStructure;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     public function boot()
     {
         $this->commands([
             MakeEnsoStructure::class,
         ]);
+
+        $this->loadDependencies()
+            ->publishDependencies();
     }
 
     public function register()
     {
         //
+    }
+
+    private function loadDependencies()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/model.php', 'enso.structures.model');
+        $this->mergeConfigFrom(__DIR__ . '/config/menu.php', 'enso.structures.menu');
+        $this->mergeConfigFrom(__DIR__ . '/config/permissionGroup.php', 'enso.structures.permissionGroup');
+        $this->mergeConfigFrom(__DIR__ . '/config/permissions.php', 'enso.structures.permissions');
+        $this->mergeConfigFrom(__DIR__ . '/config/files.php', 'enso.structures.files');
+
+        return $this;
+    }
+
+    private function publishDependencies()
+    {
+        $this->publishes([
+            __DIR__ . '/config' => config_path('enso/structures'),
+        ], 'structuremanager-config');
+
+        $this->publishes([
+            __DIR__ . '/config' => config_path('enso/structures'),
+        ], 'enso-config');
     }
 }
