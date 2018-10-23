5767e3839f5b459ab0511bae237ae7ac313264f7
Mihai <mihai.ocneanu@gmail.com>
2018-10-23 10:41:34 +0000

    Apply fixes from StyleCI

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -7,7 +7,6 @@ use LaravelEnso\StructureManager\app\Commands\MakeEnsoStructure;
 
 class AppServiceProvider extends ServiceProvider
 {
-
     public function boot()
     {
         $this->commands([
@@ -25,11 +24,11 @@ class AppServiceProvider extends ServiceProvider
 
     private function loadDependencies()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/model.php', 'enso.structures.model');
-        $this->mergeConfigFrom(__DIR__ . '/config/menu.php', 'enso.structures.menu');
-        $this->mergeConfigFrom(__DIR__ . '/config/permissionGroup.php', 'enso.structures.permissionGroup');
-        $this->mergeConfigFrom(__DIR__ . '/config/permissions.php', 'enso.structures.permissions');
-        $this->mergeConfigFrom(__DIR__ . '/config/files.php', 'enso.structures.files');
+        $this->mergeConfigFrom(__DIR__.'/config/model.php', 'enso.structures.model');
+        $this->mergeConfigFrom(__DIR__.'/config/menu.php', 'enso.structures.menu');
+        $this->mergeConfigFrom(__DIR__.'/config/permissionGroup.php', 'enso.structures.permissionGroup');
+        $this->mergeConfigFrom(__DIR__.'/config/permissions.php', 'enso.structures.permissions');
+        $this->mergeConfigFrom(__DIR__.'/config/files.php', 'enso.structures.files');
 
         return $this;
     }
@@ -37,11 +36,11 @@ class AppServiceProvider extends ServiceProvider
     private function publishDependencies()
     {
         $this->publishes([
-            __DIR__ . '/config' => config_path('enso/structures'),
+            __DIR__.'/config' => config_path('enso/structures'),
         ], 'structuremanager-config');
 
         $this->publishes([
-            __DIR__ . '/config' => config_path('enso/structures'),
+            __DIR__.'/config' => config_path('enso/structures'),
         ], 'enso-config');
     }
 }
