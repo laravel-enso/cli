90c2fa9c5009441a915980682039b3d602734619
mishu <mishu@xtelecom.ro>
2020-04-06 18:38:07 +0300

    updates assertion to be compatible with phpunit10

--- tests/features/Writers/RouteTest.php
+++ tests/features/Writers/RouteTest.php
@@ -119,7 +119,7 @@ class RouteTest extends TestCase
 
         $this->write(Routes::class);
 
-        $this->assertFileNotExists($this->viewRoutePath('perm/group/destroy.js'));
+        $this->assertFileDoesNotExists($this->viewRoutePath('perm/group/destroy.js'));
     }
 
     /** @test */
@@ -129,7 +129,7 @@ class RouteTest extends TestCase
 
         $this->write(Routes::class);
 
-        $this->assertFileNotExists($this->viewRoutePath('perm/group/show.js'));
+        $this->assertFileDoesNotExists($this->viewRoutePath('perm/group/show.js'));
     }
 
     /** @test */
@@ -154,6 +154,4 @@ class RouteTest extends TestCase
             'permissions' => [],
         ]);
     }
-
-
 }
